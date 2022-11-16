d51c26025f432d4ea7b222ff4b34f2b1ed7f792a
Brooke Luther <brookeluther8@gmail.com>
2022-11-16 16:06:29 -0500

    jquery add dvd

--- js/home.js
+++ js/home.js
@@ -43,3 +43,50 @@ function loadDvds() {
 function clearDvdTable() {
     $('#contentRows').empty();
 }
+
+function addDVD() {
+  $("#addDvdButton").click(function (event) {
+    var haveValidationErrors = checkAndDisplayValidationErrors(
+      $("#addForm").find("input")
+    );
+    if (haveValidationErrors) {
+      return false;
+    }
+
+    // Ajax call to POST data to API service
+    $.ajax({
+      type: "POST",
+      url: 'http://dvd-library.us-east-1.elasticbeanstalk.com/dvds',
+
+      // format data in JSON
+      data: JSON.stringify({
+        firstName: $("#addTitle").val(),
+        lastName: $("#addReleaseYear").val(),
+        company: $("#addDirector").val(),
+        phone: $("#addRating").val(),
+        email: $("#addNotes").val(),
+      }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      dataType: "json",
+      success: function () {
+        $("#errorMessages").empty();
+        $("#addTitle").val("");
+        $("#addReleaseYear").val("");
+        $("#addDirector").val("");
+        $("#addRating").val("");
+        $("#addNotes").val("");
+        loadDvds();
+      },
+      error: function () {
+        $("#errorMessages").append(
+          $("<li>")
+            .attr({ class: "list-group-item list-group-item-danger" })
+            .text("Error calling web service. Please try again later.")
+        );
+      },
+    });
+  });
+}
