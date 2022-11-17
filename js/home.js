2b0ac31c34a9fb164a14e9023821baa856feff5c
Masoumi-shima <masoumi.shimaa@gmail.com>
2022-11-17 11:02:39 -0500

    Edit the rating thing

--- js/home.js
+++ js/home.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     loadDvds();
     searchDvd();
+    addDVD();
 });
 
 function loadDvds() {
@@ -83,4 +84,174 @@ function hideDetails()
 {
     $('#dvdPage').hide();
     $('#dvdTableDiv').show();
+}
+
+function hideAddDvdForm() {
+    $("#errorMessages").empty();
+  
+    $("#addTitle").val("");
+    $("#addReleaseYear").val("");
+    $("#addDirector").val("");
+    $("#addRating").val("");
+    $("#addNotes").val("");
+    $('#addDvdButtonDiv').show();
+    $("#dvdTableDiv").show();
+    $("#addDvdDiv").hide();
+  }
+  
+  function addDVD() {
+    $("#addButton").click(function (event) {
+    //   var haveValidationErrors = checkAndDisplayValidationErrors(
+    //     $("#addForm").find("input")
+    //   );
+    //   if (haveValidationErrors) {
+    //     return false;
+    //   }
+      showAddDvdForm();
+      // Ajax call to POST data to API service
+      $.ajax({
+        type: "POST",
+        url: 'http://dvd-library.us-east-1.elasticbeanstalk.com/dvd',
+  
+        // format data in JSON
+        data: JSON.stringify({
+            title: $('#addTitle').val(),
+            releaseYear: $('#addReleaseYear').val(),
+            director: $('#addDirector').val(),
+            rating: $('#addRating').val(),
+            notes: $('#addNotes').val()
+        }),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        dataType: "json",
+        success: function () {
+          $("#errorMessages").empty();
+          $("#addTitle").val("");
+          $("#addReleaseYear").val("");
+          $("#addDirector").val("");
+          $("#addRating").val("");
+          $("#addNotes").val("");
+          loadDvds();
+        },
+        error: function () {
+          $("#errorMessages").append(
+            $("<li>")
+              .attr({ class: "list-group-item list-group-item-danger" })
+              .text("Error calling web service. Please try again later.")
+          );
+        },
+      });
+    });
+  }
+  
+  function showEditForm(dvdId) {
+    $("#errorMessages").empty();
+  
+    $.ajax({
+      type: "GET",
+      url:
+        "http://dvd-library.us-east-1.elasticbeanstalk.com/dvds" + dvdId,
+      success: function (data, status) {
+        $("#editTitle").val(data.title);
+        $("#editReleaseYear").val(data.releaseYear);
+        $("#editDirector").val(data.director);
+        $("#editRating").val(data.rating);
+        $("#editNotes").val(data.notes);
+        $("#editDvdId").val(data.dvdId);
+  
+      },
+      error: function () {
+        $("#errorMessages").append(
+          $("<li>")
+            .attr({ class: "list-group-item list-group-item-danger" })
+            .text("Error calling web service. Please try again later.")
+        );
+      },
+    });
+  
+    // hide the table when the form is opened
+    $("#dvdTableDiv").hide();
+    $("#editFormDiv").show();
+  }
+  
+  function hideEditForm() {
+    $("#errorMessages").empty();
+  
+    $("#editTitle").val("");
+    $("#editReleaseYear").val("");
+    $("#editDirector").val("");
+    $("#editRating").val("");
+    $("#editNotes").val("");
+  
+    $("dvdTableDiv").show();
+    $("#editFormDiv").hide();
+  }
+  
+  function updateDvd(dvdId) {
+    $("#updateButton").click(function (event) {
+      // check for errors and abort if errors are found
+      var haveValidationErrors = checkAndDisplayValidationErrors(
+        $("#editForm").find("input")
+      );
+  
+      if (haveValidationErrors) {
+        return false;
+      }
+  
+      $.ajax({
+        type: "PUT",
+        url:
+          "http://dvd-library.us-east-1.elasticbeanstalk.com/dvds" +
+          $("#editDvdId").val(),
+  
+        // format the data in JSON
+        data: JSON.stringify({
+          title: $("#editTitle").val(),
+          releaseYear: $("#editReleaseYear").val(),
+          director: $("#editDirector").val(),
+          rating: $("#editRating").val(),
+          notes: $("#editNotes").val()
+        }),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        dataType: "json",
+        success: function () {
+          $("#errorMessage").empty();
+          hideEditForm();
+          loadDvds();
+        },
+        error: function () {
+          $("#errorMessages").append(
+            $("<li>")
+              .attr({ class: "list-group-item list-group-item-danger" })
+              .text("Error calling web service. Please try again later.")
+          );
+        },
+      });
+    });
+  }
+  
+  function deleteDvd(dvdId) {
+    var answer=confirm('Do you want to delete?');
+    if(answer){
+        $.ajax({
+        type: "DELETE",
+        url:
+            "http://dvd-library.us-east-1.elasticbeanstalk.com/dvd/" + dvdId,
+        success: function () {
+            loadDvds();
+        },
+        });
+    }
+  }
+  
+function showAddDvdForm()
+{
+    $('#addDvdDiv').show();
+    $('#addDvdButtonDiv').hide();
+    $('#dvdTableDiv').hide();
 }
